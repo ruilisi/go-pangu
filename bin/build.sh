fa49a9ade1c3d109dd9d21b43671c0f6bd9d01d5
wsq <wsq@paiyou.co>
2020-07-29 18:30:03 +0800

    fix build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,4 @@
-GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -ldflags '-w -s' -o invoice
+GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -tags release -ldflags '-w -s' -o invoice
 docker build -t invoice-server .
 docker tag invoice-server ccr.ccs.tencentyun.com/ruilisi/invoice-server
 docker push ccr.ccs.tencentyun.com/ruilisi/invoice-server
--- conf/conf.go
+++ conf/conf.go
@@ -1,31 +1,13 @@
 package conf
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/spf13/viper"
 )
 
 var DEVICES = map[string]bool{"WINDOWS": true, "MAC": true, "ANDROID": true, "IOS": true}
 
-func init() {
-	viper.SetConfigType("yaml") // or viper.SetConfigType("YAML")
-	// any approach to require this configuration into your program.
-	data, err := ioutil.ReadFile("application.yml")
-	if err != nil {
-		panic(err)
-	}
-
-	viper.ReadConfig(bytes.NewBuffer(data))
-}
-
 func GetEnv(env string) string {
-	if viper.Get("GIN_ENV").(string) != "production" {
-		return fmt.Sprintf("%v", viper.Get(env))
-	} else {
-		return os.Getenv(env)
-	}
+	return fmt.Sprintf("%v", viper.Get(env))
 }
